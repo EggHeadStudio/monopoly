1efdb392dfe3f5b653d3f05911d7c80e56ab3707
Allar Hill-Tassa <allar.tassa@murata.com>
2026-09-25 14:57:30 +0300

    Connect Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Replace these values with the config shown in Firebase Console:
 // Project settings -> Your apps -> Web app -> SDK setup and configuration -> Config
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyBIAAE8JOP1z_wtOzCHcZzNZ8OWybeTJ58",
+  authDomain: "monopoly-20467.firebaseapp.com",
+  projectId: "monopoly-20467",
+  storageBucket: "monopoly-20467.firebasestorage.app",
+  messagingSenderId: "659757282829",
+  appId: "1:659757282829:web:76c00469132793003a8744"
 };
